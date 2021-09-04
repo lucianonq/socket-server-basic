1619f3108101d960ca865fc8940391b3710ae4dc
Luciano Quinteros <luciano.dekno@gmail.com>
2021-09-04 20:07:23 -0300

    Agrego app use

--- models/server.js
+++ models/server.js
@@ -19,6 +19,14 @@ class Server {
             origin: "*",
             methods: ["GET", "POST"]
           });
+
+        this.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            res.header("Access-Control-Allow-Headers", "Content-Type");
+            res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+            next();
+        });
     }
 
     middlewares() {
